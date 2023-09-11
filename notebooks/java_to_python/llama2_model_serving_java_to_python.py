f209f590817347c402f9305370733aa2b354836a
rafaelvp-db <rafaelvp-db>
2023-09-11 16:48:12 +0000

    clean

--- notebooks/java_to_python/llama2_model_serving_java_to_python.py
+++ notebooks/java_to_python/llama2_model_serving_java_to_python.py
@@ -1,23 +1,23 @@
 # Databricks notebook source
-databricks_url = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiUrl().getOrElse(None)
-token = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiToken().getOrElse(None)
-url = databricks_url + "/serving-endpoints/llama2/invocations"
-
-# COMMAND ----------
-
 # MAGIC %md
 # MAGIC
+# MAGIC ## Download Data
 # MAGIC ### AVATAR: Java-Python Program Translation Dataset
 
 # COMMAND ----------
 
-cpp_code = """
-int i = 0;
-while (i < 5) {
-  cout << i << "\n";
-  i++;
-}
-"""
+USER = dbutils.notebook.entry_point.getDbutils().notebook().getContext().userName().get()
+SCRIPT_PATH = f"/Workspace/Repos/{USER}/db-ancient-code-translation/notebooks/java_to_python/utils/download.sh"
+DOWNLOAD_PATH = "/tmp"
+FULL_PATH = f"{DOWNLOAD_PATH}/transcoder_evaluation_gfg/java"
+
+!cd {DOWNLOAD_PATH} && bash {SCRIPT_PATH}
+
+# COMMAND ----------
+
+databricks_url = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiUrl().getOrElse(None)
+token = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiToken().getOrElse(None)
+url = databricks_url + "/serving-endpoints/llama2/invocations"
 
 # COMMAND ----------
 
@@ -30,26 +30,34 @@ from pygments.lexers import PythonLexer
 from pygments.formatters import HtmlFormatter
 
 PROMPT_TEMPLATE = """
-  Assume you are an expert both in Python and C++. The code below is in C++. Please re-write in the Python language, keeping the same logic. Replace functions from C++ with the equivalent in Python that makes the most sense.\n
+  Assume you are an expert both in {input_language} and {output_language}. The code below is written in {input_language}. Please re-write in the {output_language} language, keeping the same logic. Replace methods and library imports from {input_language} with the equivalent in {output_language} that makes the most sense.\n
 """
 
 def translate(
+  code: str,
+  input_language = "Java",
+  output_language = "Python",
   prompt_template: str = PROMPT_TEMPLATE,
-  code: str = cpp_code,
-  max_tokens: int = 1000
+  max_tokens: int = 30000
 ):
+  prompt_template = prompt_template.format(
+    input_language = input_language,
+    output_language = output_language
+  )
 
   code_template = f"""
-  ### C++ Code:
-  ```cpp
+  ### Input Code:
+  ```
+
   {code}
+  
   ```
 
-  Please re-write the code above in Python language.
+  Please re-write the code above in {output_language} language. Include your answer inside the markdown block below.
 
-  ### Python code:
-  ```python
+  ### Output code:
   ```
+
   """
 
   prompt = prompt_template + code_template
@@ -82,9 +90,9 @@ def translate(
 
   return response.json()["predictions"][0]
 
-def post_process(code: str, format_: str = "markdown", style="one-dark", linenos = False):
+def render_html(code: str, style="one-dark", linenos = False):
 
-  python_code = code.split("```python")[1].split("```")[0]
+  python_code = code.split("```")[1].split("```")[0]
   html = highlight(
     python_code,
     PythonLexer(),
@@ -99,8 +107,20 @@ def post_process(code: str, format_: str = "markdown", style="one-dark", linenos
 
 # COMMAND ----------
 
-py_code = translate()
-post_process(py_code)
+import glob
+import random
+
+java_examples = glob.glob(f"{FULL_PATH}/*.java")
+example_idx = random.randint(0, len(java_examples) - 1)
+java_example = java_examples[example_idx]
+
+with open(java_example, "r") as file:
+  java_code = file.read()
+  py_code = translate(code = java_code, max_tokens = 100000)
+
+# COMMAND ----------
+
+print(py_code)
 
 # COMMAND ----------
 
